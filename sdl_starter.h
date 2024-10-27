7bb52691758bfc422b5d1fa1ce0a9b128f1c431e
Knight <karvin_jimenez@hotmail.com>
2024-10-27 01:10:15 -0400

    extra changes.

--- sdl_starter.h
+++ sdl_starter.h
@@ -4,15 +4,15 @@
 #include <iostream>
 
 // psp = 1, vita = 2
-const int scale = 1;
+const int scale = 2;
 
 //psp
-const int CELL_SIZE = 11;
-const int CELL_COUNT = 25;
+// const int CELL_SIZE = 11;
+// const int CELL_COUNT = 25;
 
 //vita
-// const int CELL_SIZE = 23;
-// const int CELL_COUNT = 23;
+const int CELL_SIZE = 21;
+const int CELL_COUNT = 26;
 
 const int SCREEN_WIDTH = CELL_SIZE * CELL_COUNT;
 const int SCREEN_HEIGHT = CELL_SIZE * CELL_COUNT;
